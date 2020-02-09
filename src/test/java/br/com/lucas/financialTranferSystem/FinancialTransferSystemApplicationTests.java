04956a7cfde1afb479388844e9d1c23cd64dcb2b
Lucas Gil <lucasgil@MacBook-Pro-de-Lucas.local>
2020-02-09 03:52:06 -0300

    Initial Commit

---
+++ src/test/java/br/com/lucas/financialTranferSystem/FinancialTransferSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucas.financialTranferSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialTransferSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
